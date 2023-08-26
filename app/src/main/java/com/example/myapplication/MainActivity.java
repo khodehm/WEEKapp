c8820e7c209780639f0df219304baf56415aa8af
Alireza Gholami <68964836+AlirezaTS@users.noreply.github.com>
2023-08-26 18:35:44 +0330

    Fix Mistakes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
     public EditText ed;
     public TextView tv;
     String Day;
-     int [] day = { 1 , 2, 3 ,4 ,5 ,6 , 7};
-
+    //TODO CHERA ???
+    //int [] day = { 1 , 2, 3 ,4 ,5 ,6 , 7};
+    ////TODO CHERA ???
     //public int[]week = new int[7];
     /*
 
@@ -38,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.Tv1);
         ed = findViewById(R.id.ED);
-        Intent main=new Intent(this,MainActivity.class);
-        startActivity(main);
+        //TODO CHERA ???
+        //Intent main=new Intent(this,MainActivity.class);
+        //startActivity(main);
+//TODO Chera ???
 /*
     switch ([] day) {
 
@@ -58,19 +61,51 @@ public class MainActivity extends AppCompatActivity {
 */
 
     }
-
+    
     public void click(View view) {
         findViewById(R.id.BTN);
-        Day = ed.getText().toString();
-
+        int integerDay = Integer.parseInt(Day);
+        String naam ;
+        switch(integerDay){
+            case 1:
+                naam  = "شنبه" ;
+            break;
+            case 2:
+                naam  = "یک شنبه" ;
+            break;
+            case 3:
+                naam  = "دو شنبه" ;
+            break;
+            case 4:
+                naam  = "سه شنبه" ;
+            break;
+            case 5:
+                naam  = "چهار شنبه" ;
+            break;
+            case 6:
+                naam  = "پنج شنبه" ;
+            break;
+            case 7:
+                naam  = "جمعه" ;
+            break;
+        }
+        nextActivity(naam);
+    }
+    private void nextActivity(String name){
+        Intent intent = new Intent (MainActivity.this,nextAct.class):
+        intent.putExtra("rooz",name);
+        startActivity(intent);
     }
 
-    public  void intent_days(int [] day ) {
+    //TODO to activity nextAct Bundle dorost kon baad rooz ro begir va to text view oon activity neshoonesh bedeh.
+
+    //TODO CHERA ???
+    /*public  void intent_days(int [] day ) {
 
     if( day[0] == 1 ) {
         Intent intent = new Intent(this, saturday.class);
         startActivity(intent);
     }
-}
+}*/
 
 }
