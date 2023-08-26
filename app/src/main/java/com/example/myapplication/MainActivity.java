3944eee80e3501ac235679f52efbbffc04ee8e04
alireza hamidi <127328597+khodehm@users.noreply.github.com>
2023-08-26 12:05:04 +0330

    1402/06/02

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,67 +5,72 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     public EditText ed;
-    public TextView sa;
+    public TextView tv;
     String Day;
-    Button Clk;
-    public EditText getEd() {
-        return ed;
-    }
+     int [] day = { 1 , 2, 3 ,4 ,5 ,6 , 7};
+
+    //public int[]week = new int[7];
+    /*
+
+    create new proj
+    copy all xmls
+    array list for days
+    just two classes for Sat Sun
+    set intent
+
+
+
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sa = findViewById(R.id.Tv1);
+        tv = findViewById(R.id.Tv1);
         ed = findViewById(R.id.ED);
+        Intent main=new Intent(this,MainActivity.class);
+        startActivity(main);
+/*
+    switch ([] day) {
 
-        Intent S = new Intent(this, saturday.class);
-        S.putExtra("day one of week ",1);
-        Intent Su = new Intent(this, sunday.class);
-        switch ("Day") {
-            case ("saturday"):
-                if (Day == "1") {
-                    startActivity(S);
-                }
+            case 1:
+
                     break;
-            case ("sunday"):
-                if (Day == "2") {
-                    startActivity(Su);
-                }
+            }
+            case 2:
+                Intent intent1 = new Intent(this, sunday.class);
+                startActivity(intent1);
                 break;
-            case ("mondday"):
-                Day = "3";
-                break;
-            case ("tuseday"):
-                Day = "4";
-                break;
-            case ("wedensday"):
-                Day = "5";
-                break;
-            case ("tursday"):
-                Day = "6";
-                break;
-            case ("friday"):
-                Day = "7";
-                break;
+            case 3:
+                Intent intent2 = new Intent(this, monday.class);
+                startActivity(intent2);
         }
+*/
+
     }
 
     public void click(View view) {
         findViewById(R.id.BTN);
-       Day= ed.getText().toString();
+        Day = ed.getText().toString();
 
     }
 
-    public void week() {
+    public  void intent_days(int [] day ) {
+
+    if( day[0] == 1 ) {
         Intent intent = new Intent(this, saturday.class);
         startActivity(intent);
-        intent.getExtras();
     }
+}
+
 }
