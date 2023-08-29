a3fc5884e4e5ba5fbb090f2e3e9b73b5dbed42d6
alireza hamidi <127328597+khodehm@users.noreply.github.com>
2023-08-29 09:10:08 +0330

    week
    week

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,104 +8,65 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //edit value
     public EditText ed;
     public TextView tv;
-    String Day;
+
+    String name;
     //TODO CHERA ???
-    //int [] day = { 1 , 2, 3 ,4 ,5 ,6 , 7};
     ////TODO CHERA ???
-    //public int[]week = new int[7];
-    /*
-
-    create new proj
-    copy all xmls
-    array list for days
-    just two classes for Sat Sun
-    set intent
-
-
-
-     */
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO CHERA ???
+        //TODO Chera ???
         tv = findViewById(R.id.Tv1);
         ed = findViewById(R.id.ED);
-        //TODO CHERA ???
-        //Intent main=new Intent(this,MainActivity.class);
-        //startActivity(main);
-//TODO Chera ???
-/*
-    switch ([] day) {
 
+    }
+    public void click(View view) {
+       name= ed.getText().toString();
+
+        int integerDay = Integer.parseInt(name);
+        switch (integerDay) {
             case 1:
-
-                    break;
-            }
+                name= "شنبه";
+                break;
             case 2:
-                Intent intent1 = new Intent(this, sunday.class);
-                startActivity(intent1);
+                name = "یک شنبه";
                 break;
             case 3:
-                Intent intent2 = new Intent(this, monday.class);
-                startActivity(intent2);
-        }
-*/
-
-    }
-    
-    public void click(View view) {
-        findViewById(R.id.BTN);
-        int integerDay = Integer.parseInt(Day);
-        String naam ;
-        switch(integerDay){
-            case 1:
-                naam  = "شنبه" ;
-            break;
-            case 2:
-                naam  = "یک شنبه" ;
-            break;
-            case 3:
-                naam  = "دو شنبه" ;
-            break;
+                name = "دو شنبه";
+                break;
             case 4:
-                naam  = "سه شنبه" ;
-            break;
+                name = "سه شنبه";
+                break;
             case 5:
-                naam  = "چهار شنبه" ;
-            break;
+                name = "چهار شنبه";
+                break;
             case 6:
-                naam  = "پنج شنبه" ;
-            break;
+                name = "پنج شنبه";
+                break;
             case 7:
-                naam  = "جمعه" ;
-            break;
+                name = "جمعه";
+                break;
         }
-        nextActivity(naam);
+        Intent intent=new Intent();
+        nextActivity(name);
     }
-    private void nextActivity(String name){
-        Intent intent = new Intent (MainActivity.this,nextAct.class):
-        intent.putExtra("rooz",name);
+
+    private void nextActivity(String name) {
+        Intent intent = new Intent(MainActivity.this, saturday.class);
+        intent.putExtra("Day", name);
         startActivity(intent);
     }
 
     //TODO to activity nextAct Bundle dorost kon baad rooz ro begir va to text view oon activity neshoonesh bedeh.
 
     //TODO CHERA ???
-    /*public  void intent_days(int [] day ) {
-
-    if( day[0] == 1 ) {
-        Intent intent = new Intent(this, saturday.class);
-        startActivity(intent);
-    }
-}*/
-
 }
--- app/src/main/java/com/example/myapplication/saturday.java
+++ app/src/main/java/com/example/myapplication/saturday.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class saturday extends AppCompatActivity {
-
+    String day;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saturday);
-        Intent intent = new Intent(this, sunday.class);
+        Bundle bundle = getIntent().getExtras();
+        day = bundle.getString("Day");
+        tv.setText(day);
 
     }
 }
